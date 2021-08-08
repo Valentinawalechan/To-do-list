512a8379d52604b63ff7f1f4fec9612d27e35fc0
Valentinawalechan <vale.disa86@gmail.com>
2021-08-08 23:20:23 +0100

    add new components
    added "AddNewTask" and "Todos" components

--- src/AddNewTask.js
+++ src/AddNewTask.js
@@ -2,10 +2,23 @@ import React from "react";
 import "./AddNewTask.css";
 
 export default function AddNewTask () {
+    function addTask (event) {
+        event.preventDefault();
+        alert("newTask");
+    }
+
     return (
+        <div>
         <form>
-            <input type="search" placeholder="Type a new task" />
-            <button type="submit"> Add </button> 
+        <div className="row">
+        <div className="col-8">
+        <input type="search" placeholder="Type a new task..." />
+        </div>
+        <div className="col-4">
+            <input type="submit" class="btn btn-primary" value="Add" OnClick={addTask} />
+        </div>
+        </div>
         </form>
+        </div>
     )
 }
--- src/Todos.js
+++ src/Todos.js
@@ -5,9 +5,11 @@ import Tasks from "./Tasks";
 
 export default function Todos () {
     return (
-        <div>
+        <div className="todos">
         <h1>What do you have to do?</h1>
         <AddNewTask />
+        <br />
+        <h2>Remaining Tasks</h2>
         <Tasks />
         </div>
         )
